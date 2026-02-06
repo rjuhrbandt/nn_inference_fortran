9fb1f96afc234d8d203bf3ee506de7f686994348
Rajka Juhrbandt <rajka.juhrbandt@awi.de>
2026-02-06 10:21:50 +0100

    Cosmetic changes involving capitalization

--- main.f90
+++ main.f90
@@ -6,73 +6,73 @@
 ! 1. Load the neural network weights and biases from the binary files in ./weights
 ! 2. Load test inputs and reference outputs from ./test_io
 ! 3. For each test case, run the Fortran forward pass and compare outputs
-! 4. Report PASS/FAIL for each test case and overall summary: PASS if difference between Fortran and Python outputs is < 1e-5, else FAIL
+! 4. Report PASS/FAIL for each test case and overall summary: PASS IF dIFference between Fortran and Python outputs is < 1e-5, ELSE FAIL
 
 PROGRAM nn_inference
-  use nn_module
-  implicit none
+  USE nn_module
+  IMPLICIT NONE
 
-  integer :: nlayers, u, i
-  real, allocatable :: input(:), output(:), ref_output(:)
-  real :: max_err
-  integer :: num_tests
-  character(len=100) :: fname
-  character(len=32) :: nlname='weights/nlayers.bin', nnname='weights/nneurons.bin'
-  integer, allocatable :: layer_sizes(:)
+  INTEGER :: nlayers, u, i
+  REAL, ALLOCATABLE :: input(:), output(:), ref_output(:)
+  REAL :: max_err
+  INTEGER :: num_tests
+  CHARACTER(LEN=100) :: fname
+  CHARACTER(LEN=32) :: nlname='weights/nlayers.bin', nnname='weights/nneurons.bin'
+  INTEGER, ALLOCATABLE :: layer_sizes(:)
 
-  ! Step 1: Read architecture to determine sizes
-  ! write(*,*) 'Reading neural network architecture...'
-  call read_nn_architecture(nlname, nnname, nlayers, layer_sizes)
-  ! write(*,*) 'Found nlayers: ', nlayers
-  ! write(*,*) 'Found layer sizes: ', layer_sizes
+  ! Step 1: READ architecture to determine sizes
+  ! WRITE(*,*) 'READing neural network architecture...'
+  CALL READ_nn_architecture(nlname, nnname, nlayers, layer_sizes)
+  ! WRITE(*,*) 'Found nlayers: ', nlayers
+  ! WRITE(*,*) 'Found layer sizes: ', layer_sizes
 
   ! Iterate through num_tests test cases
-  write(fname, '(A)') 'test_io/num_tests.bin'
-  open(newunit=u, file=trim(fname), status='old', access='stream', form='unformatted', action='read')
-  read(u) num_tests
-  close(u)
-  ! write(*,*) 'Number of test cases: ', num_tests
+  WRITE(fname, '(A)') 'test_io/num_tests.bin'
+  OPEN(newunit=u, file=trim(fname), status='old', access='stream', form='unformatted', action='READ')
+  READ(u) num_tests
+  CLOSE(u)
+  ! WRITE(*,*) 'Number of test cases: ', num_tests
 
-  ! Allocate array for inputs and outputs
-  allocate(input(layer_sizes(1)))
-  allocate(ref_output(layer_sizes(nlayers+1)))
+  ! ALLOCATE array for inputs and outputs
+  ALLOCATE(input(layer_sizes(1)))
+  ALLOCATE(ref_output(layer_sizes(nlayers+1)))
 
   DO i=0, num_tests-1
-    write(*,*) 'Running test case ', i
+    WRITE(*,*) 'Running test case ', i
 
-    ! Step 2: Read inputs and outputs
-    ! write(*,*) 'Reading input array...'
-    write(fname, '(A,I0,A)') 'test_io/input_', i, '.bin'
-    open(newunit=u, file=trim(fname), access='stream', form='unformatted', status='old')
-    read(u) input
-    close(u)
-    ! write(*,*) 'Reading output array...'
-    write(fname, '(A,I0,A)') 'test_io/output_', i, '.bin'
-    open(newunit=u, file=trim(fname), access='stream', form='unformatted', status='old')
-    read(u) ref_output
-    close(u)
+    ! Step 2: READ inputs and outputs
+    ! WRITE(*,*) 'READing input array...'
+    WRITE(fname, '(A,I0,A)') 'test_io/input_', i, '.bin'
+    OPEN(newunit=u, file=trim(fname), access='stream', form='unformatted', status='old')
+    READ(u) input
+    CLOSE(u)
+    ! WRITE(*,*) 'READing output array...'
+    WRITE(fname, '(A,I0,A)') 'test_io/output_', i, '.bin'
+    OPEN(newunit=u, file=trim(fname), access='stream', form='unformatted', status='old')
+    READ(u) ref_output
+    CLOSE(u)
 
     ! Step 3: Perform the forward pass for each layer
-    call forward_pass_full(input, nlayers, layer_sizes, output)
+    CALL forward_pass_full(input, nlayers, layer_sizes, output)
     
     ! Compare outputs and report PASS/FAIL
-    ! write(*,*) 'Comparing outputs...'
+    ! WRITE(*,*) 'Comparing outputs...'
     max_err = maxval(abs(output - ref_output))
-    write(*,*) 'Output from inference: ', output
-    write(*,*) 'Reference output:     ', ref_output
-    write(*,*) 'Maximum absolute error: ', max_err
-    if (max_err < 1e-5) then
-      ! write(*, '(A,I0,A,F10.6)') 'Test case: PASS (max error = ', max_err, ')'
-      write(*,*) 'Test case: PASS'
-    else
-      ! write(*, '(A,I0,A,F10.6)') 'Test case: FAIL (max error = ', max_err, ')'
-      write(*,*) 'Test case: FAIL'
-    end if
+    WRITE(*,*) 'Output from inference: ', output
+    WRITE(*,*) 'Reference output:     ', ref_output
+    WRITE(*,*) 'Maximum absolute error: ', max_err
+    IF (max_err < 1e-5) THEN
+      ! WRITE(*, '(A,I0,A,F10.6)') 'Test case: PASS (max error = ', max_err, ')'
+      WRITE(*,*) 'Test case: PASS'
+    ELSE
+      ! WRITE(*, '(A,I0,A,F10.6)') 'Test case: FAIL (max error = ', max_err, ')'
+      WRITE(*,*) 'Test case: FAIL'
+    END IF
 
   END DO
     
-  deallocate(input)
-  deallocate(ref_output)
-  deallocate(layer_sizes)
+  DEALLOCATE(input)
+  DEALLOCATE(ref_output)
+  DEALLOCATE(layer_sizes)
 
 END PROGRAM nn_inference
--- nn_module.f90
+++ nn_module.f90
@@ -1,78 +1,78 @@
 ! Steps to read the architecture and weights for a neural network and do inference on test data.
 
-module nn_module
-    implicit none
-    private
-    public :: read_nn_architecture, read_nn_weights, read_nn_biases, read_nn_activation, forward_pass_single, forward_pass_full
+MODULE nn_module
+    IMPLICIT NONE
+    PRIVATE
+    PUBLIC :: read_nn_architecture, read_nn_weights, read_nn_biases, read_nn_activation, forward_pass_single, forward_pass_full
 
-    contains
+    CONTAINS
 
-        subroutine read_nn_architecture(nlname, nnname, nl, layer_sizes)
-            character(len=32) :: nlname, nnname
-            integer, intent(out) :: nl
-            integer, allocatable, intent(out) :: layer_sizes(:)
-            integer :: iunit
+        SUBROUTINE read_nn_architecture(nlname, nnname, nl, layer_sizes)
+            CHARACTER(LEN=32) :: nlname, nnname
+            INTEGER, INTENT(OUT) :: nl
+            INTEGER, ALLOCATABLE, INTENT(OUT) :: layer_sizes(:)
+            INTEGER :: iunit
 
-            write(nlname, '(A)') 'weights/nlayers.bin'
-            open(newunit=iunit, file=trim(nlname), status='old', action='read', form='unformatted', access='stream')
-            read(iunit) nl
-            close(iunit)
+            WRITE(nlname, '(A)') 'weights/nlayers.bin'
+            OPEN(NEWUNIT=iunit, FILE=TRIM(nlname), STATUS='old', ACTION='read', FORM='unformatted', ACCESS='stream')
+            READ(iunit) nl
+            CLOSE(iunit)
 
-            allocate(layer_sizes(nl+1)) ! one layer is (nnneurons0, nneurons1), so we need nlayers+1 entries
+            ALLOCATE(layer_sizes(nl+1)) ! one layer is (nnneurons0, nneurons1), so we need nlayers+1 entries
 
-            write(nnname, '(A)') 'weights/nneurons.bin'
-            open(newunit=iunit, file=trim(nnname), status='old', action='read', form='unformatted', access='stream')
-            read(iunit) layer_sizes
-            close(iunit)
-        end subroutine read_nn_architecture
+            WRITE(nnname, '(A)') 'weights/nneurons.bin'
+            OPEN(NEWUNIT=iunit, FILE=TRIM(nnname), STATUS='old', ACTION='read', FORM='unformatted', ACCESS='stream')
+            READ(iunit) layer_sizes
+            CLOSE(iunit)
+        END SUBROUTINE read_nn_architecture
 
-        subroutine read_nn_weights(filename_weights, weights)
+        SUBROUTINE read_nn_weights(filename_weights, weights)
             ! Can read weights
-            character(len=*), intent(in) :: filename_weights
-            real, intent(out) :: weights(:,:) ! Dimensions: input neurons, output neurons
-            integer :: iunit
+            CHARACTER(LEN=*), INTENT(IN) :: filename_weights
+            REAL, INTENT(OUT) :: weights(:,:) ! Dimensions: input neurons, output neurons
+            INTEGER :: iunit
 
-            open(newunit=iunit, file=filename_weights, status='old', access='stream', form='unformatted')
-            read(iunit) weights
-            close(iunit)
+            OPEN(NEWUNIT=iunit, FILE=filename_weights, STATUS='old', ACCESS='stream', FORM='unformatted')
+            READ(iunit) weights
+            CLOSE(iunit)
 
-        end subroutine read_nn_weights
+        END SUBROUTINE read_nn_weights
 
-        subroutine read_nn_biases(filename_biases, biases)
+        SUBROUTINE read_nn_biases(filename_biases, biases)
             ! Can read biases
-            character(len=*), intent(in) :: filename_biases
-            real, intent(out) :: biases(:) ! Dimensions: number of input neurons
-            integer :: iunit
+            CHARACTER(LEN=*), INTENT(IN) :: filename_biases
+            REAL, INTENT(OUT) :: biases(:) ! Dimensions: number of input neurons
+            INTEGER :: iunit
 
-            open(newunit=iunit, file=filename_biases, status='old', action='read', access='stream', form='unformatted')
-            read(iunit) biases
-            close(iunit)
+            OPEN(NEWUNIT=iunit, FILE=filename_biases, STATUS='old', ACTION='read', ACCESS='stream', FORM='unformatted')
+            READ(iunit) biases
+            CLOSE(iunit)
 
-        end subroutine read_nn_biases
+        END SUBROUTINE read_nn_biases
 
-        subroutine read_nn_activation(filename, activation)
+        SUBROUTINE read_nn_activation(filename, activation)
             ! Reads activations. Routine is called layerwise, so this is only for one layer. (single string)
-            character(len=*), intent(in) :: filename
-            character(len=*), intent(out) :: activation
-            integer :: iunit
+            CHARACTER(LEN=*), INTENT(IN) :: filename
+            CHARACTER(LEN=*), INTENT(OUT) :: activation
+            INTEGER :: iunit
 
-            open(newunit=iunit, file=filename, status='old', action='read')
-            read(iunit, *) activation
-            close(iunit)
+            OPEN(NEWUNIT=iunit, FILE=filename, STATUS='old', ACTION='read')
+            READ(iunit, *) activation
+            CLOSE(iunit)
 
-        end subroutine read_nn_activation
+        END SUBROUTINE read_nn_activation
 
-        subroutine forward_pass_single(inputs, weights, biases, activation, results)
+        SUBROUTINE forward_pass_single(inputs, weights, biases, activation, results)
             ! Does the inference step for a single layer
-            real, intent(in) :: weights(:,:)
-            real, intent(in) :: biases(:)
-            character(len=16), intent(in) :: activation
-            real, intent(in) :: inputs(:)
-            real, allocatable, intent(out) :: results(:)
+            REAL, INTENT(IN) :: weights(:,:)
+            REAL, INTENT(IN) :: biases(:)
+            CHARACTER(LEN=16), INTENT(IN) :: activation
+            REAL, INTENT(IN) :: inputs(:)
+            REAL, ALLOCATABLE, INTENT(OUT) :: results(:)
 
             ! Iterate through layers (infer from size of weights)
             ! Matrix multiplication at each layer
-            allocate(results(size(weights(2,:))))
+            ALLOCATE(results(size(weights(2,:))))
             results = matmul(inputs, weights) + biases
             IF (activation == 'relu') THEN
                 ! write(*,*) 'Applying relu activation'
@@ -84,47 +84,47 @@ module nn_module
                 ! write(*,*) 'Unknown activation function:', activation
             END IF
 
-        end subroutine forward_pass_single
+        END SUBROUTINE forward_pass_single
 
-        subroutine forward_pass_full(input, nlayers, layer_sizes, output)
+        SUBROUTINE forward_pass_full(input, nlayers, layer_sizes, output)
             ! Does the full forward pass through the network
-            real, intent(in) :: input(:)
-            integer, intent(in) :: nlayers
-            integer, intent(in) :: layer_sizes(:)
-            real, allocatable, intent(out) :: output(:)
-            integer :: n, u
-            character(len=64) :: wname, bname, aname
-            real, allocatable :: current_input(:), current_output(:)
-            real, allocatable :: current_weights(:,:), current_biases(:)
-            character(len=16) :: current_activation
+            REAL, INTENT(IN) :: input(:)
+            INTEGER, INTENT(IN) :: nlayers
+            INTEGER, INTENT(IN) :: layer_sizes(:)
+            REAL, ALLOCATABLE, INTENT(OUT) :: output(:)
+            INTEGER :: n, u
+            CHARACTER(LEN=64) :: wname, bname, aname
+            REAL, ALLOCATABLE :: current_input(:), current_output(:)
+            REAL, ALLOCATABLE :: current_weights(:,:), current_biases(:)
+            CHARACTER(LEN=16) :: current_activation
 
             current_input = input
             
             DO n=0, nlayers-1
                 ! write(*,*) 'Processing layer ', n
                 ! Remove allocation from possible previous iteration
-                if (allocated(current_weights)) deallocate(current_weights)
-                if (allocated(current_biases)) deallocate(current_biases)
+                IF (allocated(current_weights)) DEALLOCATE(current_weights)
+                IF (allocated(current_biases)) DEALLOCATE(current_biases)
 
                 ! Read weights, biases, activations for layer n
                 ! write(*,*) 'Reading weights...'
-                write(wname, '(A,I0,A)') 'weights/layer_', n, '_weights.bin'
-                allocate(current_weights(layer_sizes(n+1), layer_sizes(n+2)))
-                call read_nn_weights(wname, current_weights)
+                WRITE(wname, '(A,I0,A)') 'weights/layer_', n, '_weights.bin'
+                ALLOCATE(current_weights(layer_sizes(n+1), layer_sizes(n+2)))
+                CALL read_nn_weights(wname, current_weights)
                 ! write(*,*) 'shape of current_weights after reading: ', shape(current_weights)
                 
                 ! write(*,*) 'Reading biases...'
-                write(bname, '(A,I0,A)') 'weights/layer_', n, '_biases.bin'
-                allocate(current_biases(layer_sizes(n+2)))
-                call read_nn_biases(bname, current_biases)
+                WRITE(bname, '(A,I0,A)') 'weights/layer_', n, '_biases.bin'
+                ALLOCATE(current_biases(layer_sizes(n+2)))
+                CALL read_nn_biases(bname, current_biases)
                 
                 ! write(*,*) 'Reading activation...'
-                write(aname, '(A,I0,A)') 'weights/layer_', n, '_act.txt'
-                call read_nn_activation(aname, current_activation)
+                WRITE(aname, '(A,I0,A)') 'weights/layer_', n, '_act.txt'
+                CALL read_nn_activation(aname, current_activation)
 
                 ! write(*,*) 'Layer ', n, ': weights shape = ', shape(current_weights), ', biases shape = ', shape(current_biases)
 
-                call forward_pass_single(current_input, current_weights, current_biases, current_activation, current_output)
+                CALL forward_pass_single(current_input, current_weights, current_biases, current_activation, current_output)
 
                 current_input = current_output
 
@@ -132,6 +132,6 @@ module nn_module
 
             output = current_output
 
-        end subroutine forward_pass_full
+        END SUBROUTINE forward_pass_full
 
-end module nn_module
+END MODULE nn_module
